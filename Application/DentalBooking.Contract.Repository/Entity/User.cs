7309091d51c2f26164b6f8e6fbf96bcaafcb91ef
Huy Nguyen <tuanhuy1104pro@gmail.com>
2024-09-13 17:07:29 +0700

    Initialize Project - Model

---
+++ Application/DentalBooking.Contract.Repository/Entity/User.cs
@@ -0,0 +1,17 @@
+﻿using DentalBooking.Core.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalBooking.Contract.Repository.Entity
+{
+    public class User : BaseEntity
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber {  get; set; } = string.Empty;
+
+    }
+}
