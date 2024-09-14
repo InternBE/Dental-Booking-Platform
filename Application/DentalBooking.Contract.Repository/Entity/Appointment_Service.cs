ad39c92dbfeffbae6a728fe3e4cd2c1dc0496882
Huy Nguyen <tuanhuy1104pro@gmail.com>
2024-09-14 12:38:24 +0700

    Services - Appointment services Done

--- Application/DentalBooking.Contract.Repository/Entity/Appointment_Service.cs
+++ Application/DentalBooking.Contract.Repository/Entity/Appointment_Service.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using DentalBooking.Core.Base;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DentalBooking.Contract.Repository.Entity
 {
-    public class Appointment_Service
+    public class Appointment_Service : BaseEntity
     {
+        [ForeignKey("Appointment")]
+        public int AppointmentId { get; set; }
+        public virtual Appointment? Appointment { get; set; }
+
+        [ForeignKey("Services")]
+        public int ServiceId { get; set; }
+        public virtual Services? Services { get; set; }
     }
 }
--- Application/DentalBooking.Contract.Repository/Entity/Services.cs
+++ Application/DentalBooking.Contract.Repository/Entity/Services.cs
@@ -9,5 +9,10 @@ namespace DentalBooking.Contract.Repository.Entity
 {
     public class Services : BaseEntity
     {
+        public string ServiceName { get; set; } = string.Empty;
+        public string Description {  get; set; } = string.Empty;
+        public decimal Price {  get; set; }
+        
+        public virtual ICollection<Appointment_Service>? GetAppointment_Services {  get; set; } 
     }
 }
