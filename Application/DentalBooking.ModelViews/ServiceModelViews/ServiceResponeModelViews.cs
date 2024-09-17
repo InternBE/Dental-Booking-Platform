0aebecf36a6f5ffb0426c658db6eca1fa9b6c871
Huy Nguyen <tuanhuy1104pro@gmail.com>
2024-09-17 15:16:56 +0700

    Service CRUD With UOW

---
+++ Application/DentalBooking.ModelViews/ServiceModelViews/ServiceResponeModelViews.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalBooking.ModelViews.ServiceModelViews
+{
+    public class ServiceResponeModelViews
+    {
+        public string ServiceName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
