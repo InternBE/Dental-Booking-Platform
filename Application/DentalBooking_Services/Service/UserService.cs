af4cf720a417d5324aa25894f53b8fa83bad0845
TranTrachNguyen <trantrachnguyen2312@gmail.com>
2024-09-19 21:26:50 +0700

    Update UserService.cs

--- Application/DentalBooking_Services/Service/UserService.cs
+++ Application/DentalBooking_Services/Service/UserService.cs
@@ -1,4 +1,4 @@
-﻿using DentalBooking.Contract.Repository.Entity;
+using DentalBooking.Contract.Repository.Entity;
 using DentalBooking.Contract.Repository;
 using DentalBooking.ModelViews.UserModelViews;
 using DentalBooking_Contract_Services.Interface;
@@ -51,47 +51,6 @@ public class UserService : IUserService
                 PhoneNumber = user.PhoneNumber
             };
         }
-
-    // Phương thức GetAll để trả về danh sách UserResponseModel
-    public async Task<IList<UserResponseModel>> GetAll()
-    {
-        var userRepository = _unitOfWork.GetRepository<User>();
-        var users = await userRepository.GetAllAsync();
-
-        var userResponseModels = users.Select(user => new UserResponseModel
-        {
-            Id = user.Id,
-            FullName = user.FullName,
-            Email = user.Email,
-            PhoneNumber = user.PhoneNumber,
-            ClinicId = user.ClinicId,
-        }).ToList();
-
-        return userResponseModels;
-    }
-
-    // Phương thức Create để tạo người dùng mới
-    public async Task<UserResponseModel> Create(UserRequestModel userRequest)
-    {
-        var user = new User
-        {
-            FullName = userRequest.FullName,
-            Email = userRequest.Email,
-            PhoneNumber = userRequest.PhoneNumber,
-            ClinicId = userRequest.ClinicId,
-        };
-
-        await _unitOfWork.GetRepository<User>().InsertAsync(user);
-        await _unitOfWork.SaveAsync();
-
-        return new UserResponseModel
-        {
-            Id = user.Id,
-            FullName = user.FullName,
-            Email = user.Email,
-            PhoneNumber = user.PhoneNumber
-        };
-    }
 
     // Các phương thức khác
     public Task<IEnumerable<User>> GetAllUsersAsync()
