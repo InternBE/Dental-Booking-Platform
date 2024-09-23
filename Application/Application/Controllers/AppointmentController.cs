1107827292da30ad164692c42cb45bbb51f56855
QuanHD0610 <142657399+QuanHD0610@users.noreply.github.com>
2024-09-23 22:25:34 +0700

    update appointment BookPeriodic

--- Application/Application/Controllers/AppointmentController.cs
+++ Application/Application/Controllers/AppointmentController.cs
@@ -112,5 +112,18 @@ namespace Application.Controllers
             return CreatedAtAction(nameof(GetAppointmentById), new { id = createdAppointment.AppointmentDate }, createdAppointment);
         }
 
+        // POST: api/Appointment/BookPeriodic
+        [HttpPost("BookPeriodic")]
+        public async Task<IActionResult> BookPeriodicAppointments([FromBody] AppointmentRequestModelView model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Đăng ký 12 cuộc hẹn
+            var response = await _appointmentServices.BookPeriodicAppointmentsAsync(model, 12);
+            return Ok(response);
+        }
     }
 }
